4b793c627dc018096c2ed66cf01f6fa4b0316291
unknown <wmigue@gmail.com>
2021-08-29 00:56:10 -0300

    29-8-2021

--- ASYNC-AWAIT/main.js
+++ ASYNC-AWAIT/main.js
@@ -1,13 +1,58 @@
 
 
-const traerDatos = async () => {
+datos = [{
+    "nombre": "migue",
+  }]
+  
+  
+  //usando promesas
+  const f = async (x) => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(x)
+      }, 1500)
+    })
+  }
+  
+  f(datos).then((s) => {
+    console.log(s)
+    console.log("hey")
+  }).then(() => {
+    console.log("después de hey , digo otra cosa...")
+  })
+  
+  
+  
+  
 
-    const respuesta = await fetch('https://jsonplaceholder.typicode.com/posts')
-    const texto = await respuesta.json()
-    texto.map(x => { console.log(x.title) }) //traeme todos los titulos del json.
+  
+  //usando fetch
+  const fetchData = async (url) => {
+    const r = await fetch(url)
+    const d = r.json()
+    return {
+      datosInternet: async () => d, //lo mismo a decir: {return d}
+      mensaje: 'despues de los datos, digo esto...'
+    }
+  }
+  
+  fetchData('https://jsonplaceholder.typicode.com/todos/1')
+    .then((r) => {
+      r.datosInternet().then(x => {
+        console.log(x)
+        console.log(r.mensaje)
+      })
+    })
+  
+  
+  
 
-}
 
 
-const datos = await traerDatos()
-console.log(datos)
+
+
+
+
+
+
+
